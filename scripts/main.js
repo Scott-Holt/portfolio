29401f7ec524280dc6f0e151f00faa8469adc85e
Scott <scottwesleymusic@gmail.com>
2019-06-07 16:44:10 -0500

    fixes responsive issues

--- scripts/main.js
+++ scripts/main.js
@@ -4,15 +4,13 @@ const closeIcon = document.querySelectorAll('.close-icon');
 
 function openNav() {
     nav.style.left = '0%';
-    hamIcon.style.display = 'none';
-
 }
 
 
 function closeNav(e) {
     nav.style.left = '-70%';
-    hamIcon.style.display = 'block';
 }
 
 hamIcon.addEventListener('click', openNav);
 closeIcon.forEach(item => item.addEventListener('click', closeNav));
+console.log(window.innerWidth);
