d5d84c30f874d37272480933d0f447fec9e6ea68
Scott <scottwesleymusic@gmail.com>
2019-06-07 11:58:23 -0500

    adds working mobile nav bar

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,18 @@
-console.log(123);
+const hamIcon = document.getElementById('hamburger-icon');
+const nav = document.getElementById('mobile-nav');
+const closeIcon = document.querySelectorAll('.close-icon');
+
+function openNav() {
+    nav.style.left = '0%';
+    hamIcon.style.display = 'none';
+
+}
+
+
+function closeNav(e) {
+    nav.style.left = '-70%';
+    hamIcon.style.display = 'block';
+}
+
+hamIcon.addEventListener('click', openNav);
+closeIcon.forEach(item => item.addEventListener('click', closeNav));
